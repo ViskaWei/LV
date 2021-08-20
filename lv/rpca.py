1cdcbda25286a88a08c1d19733d253e910ca841e
ViskaWei <swei20@jhu.edu>
2021-08-19 23:27:09 -0400

    debug rpca

--- lv/rpca.py
+++ lv/rpca.py
@@ -1,133 +1,142 @@
 import sys
 import time
 import numpy as np
+from tqdm import tqdm
 from numpy.linalg import svd, norm
 from multiprocessing.pool import ThreadPool
 
-def prox_l1(vec, cutoff):
-    return np.maximum(0, vec - cutoff) - np.maximum(0, -vec - cutoff)
 
-def prox_matrix(mat, cutoff, prox_f):
-    u, s, v = svd(mat, full_matrices=False)
-    prox_s = prox_f(s[:, None], cutoff)
-    return u.dot(np.diagflat(prox_s)).dot(v.T)
+class RPCA(object):
+    def __init__(self, data, la=1.0, ratio=0.15, parallel=0, n_iter=100):
+        self.X = data
+        self.N = 3
+        self.parallel = parallel
+        self.m, self.n = self.X.shape
+        self.n_iter = n_iter
+        self.tol = None
+        self.gs = None
+        self.gl = None
+        self.la = la
+        self.rho = 1.0 / self.la
 
-def avg(*args):
-    return np.mean([*args], axis=0)
+        self.init(data, ratio)
 
-def loss(R, gs, S, gl, L):
-    noise   =      norm(R, ord='fro') ** 2  # squared frobenius norm (makes X_i small)
-    sparse  = gs * norm(S.reshape(-1), ord=1)           # L1 norm (makes X_i sparse)
-    lowrank = gl * norm(L, ord="nuc")       # nuclear norm (makes X_i low rank)
+    def init(self, data, ratio):
+        gs_max = np.sum(data)
+        gl_max = norm(data, ord=2)
+        self.gs = ratio * gs_max
+        self.gl = ratio * gl_max
 
-    return noise + sparse + lowrank
+        abs_tol   = 1e-4 * np.sqrt(self.m * self.n * self.N)
+        rel_tol   = 1e-2
+        rel_tol_N = 1e-2 * np.sqrt(self.N)
+        self.tol  = [abs_tol, rel_tol, rel_tol_N]
+
+    def init_pcp(self):
+        R = np.zeros((self.m, self.n))    #Residual
+        S = np.zeros((self.m, self.n))    #Sparse components
+        L = np.zeros((self.m, self.n))    #Low rank components
+        U = np.zeros((self.m, self.n))
+        z = np.zeros((self.m, self.n * self.N))
+        print("\n%3s\t%10s\t%10s\t%10s\t%10s\t%10s" %('iter', 'err', 'eps r', 
+                                                  'dz', 'eps U', 'loss'))
+
+        h = {}
+        h['loss'] = np.zeros(self.n_iter)
+        h['err'] = np.zeros(self.n_iter)
+        h['dz'] = np.zeros(self.n_iter)
+        h['eps_r']  = np.zeros(self.n_iter)
+        h['eps_U'] = np.zeros(self.n_iter)
+        return R, S, L, U, z, h
 
 
-def rpcaADMM(data):
-    """
-    ADMM implementation of matrix decomposition. In this case, RPCA.
+    def update_R(self, R, B):
+        return (R - B) / (1.0 + self.la)
 
-    Adapted from: http://web.stanford.edu/~boyd/papers/prox_algs/matrix_decomp.html
-    """
+    def update_S(self, S, B):
+        return RPCA.prox_l1 (S - B, self.la * self.gs)
 
-    pool = ThreadPool(processes=3) # Create thread pool for asynchronous processing
+    def update_L(self, L, B):
+        return self.prox_mat(L - B, self.la * self.gl)
 
-    N = 3         # the number of matrices to split into 
-                  # (and cost function expresses how you want them)
- 
-    X = np.float_(data)    # A = S + L + V
-    m, n = X.shape
+    def prox_mat(self, mat, cutoff):
+        u, s, v = svd(mat, full_matrices=False)
+        prox_s = RPCA.prox_l1(s[:, None], cutoff)
+        if self.m >= self.n:
+            return u.dot(np.diagflat(prox_s)).dot(v.T)
+        else:
+            return u.dot(np.diagflat(prox_s)).dot(v)
 
-    gs_max = np.sum(X)      #maximum sparsity constraint
-    gl_max = norm(X, ord=2) #maximum lowrank constraint
-    gs = 0.15 * gs_max
-    gl = 0.15 * gl_max
+    def loss(self, R, S, L):
+        noise   =           norm(R, ord='fro') ** 2    # squared frobenius norm (makes X_i small)
+        sparse  = self.gs * norm(S.reshape(-1), ord=1) # L1 norm (makes X_i sparse)
+        lowrank = self.gl * norm(L, ord="nuc")         # nuclear norm (makes X_i low rank)
+        return noise + sparse + lowrank
 
-    MAX_ITER = 100
-    ABSTOL   = 1e-4 * np.sqrt(m * n * N)
-    RELTOL   = 1e-2
-    RELTOLN  = 1e-2 * np.sqrt(N)
 
-    start = time.time()
+    def episode(self, k, *args):
+        R, S, L, U, z, h = args
+        # print(R.shape, S.shape, L.shape, U.shape)
+        R, S, L, U = self.update_RSLU(R, S, L, U)
+        RSL = np.hstack((R, S, L))
+        E = (self.X - R - S - L) / 3.0
+        
+        new_z = RSL + np.tile(E, (1, 3))
 
-    lambdap = 1.0
-    rho = 1.0 / lambdap
+        h['loss'][k]  = self.loss(R, S, L)
+        h['err'][k]   = (3.0 * norm(E, 'fro')**2)**0.5
+        h['dz'][k]    = norm(self.rho * (new_z - z), 'fro')
+        h['eps_r'][k] = self.tol[0] + self.tol[1] * np.maximum(norm(RSL, 'fro'), norm(z, 'fro'))
+        h['eps_U'][k] = self.tol[0] + self.tol[2] * norm(self.rho * U, 'fro')
 
-    R = np.zeros((m, n))    #Residual
-    S = np.zeros((m, n))    #Sparse components
-    L = np.zeros((m, n))    #Low rank components
-    z = np.zeros((m, N * n))
-    U = np.zeros((m, n))
+        return R, S, L, U, new_z, h
 
-    print("\n%3s\t%10s\t%10s\t%10s\t%10s\t%10s" %('iter', 'r norm', 'eps pri', 
-                                                  's norm', 'eps dual', 'objective'))
+    def stop(self):
+        return (h['r_norm'][k] < h['eps_pri'][k]) and (h['s_norm'][k] < h['eps_dual'][k])
 
-    # Saving state
-    h = {}
-    h['loss'] = np.zeros(MAX_ITER)
-    h['r_norm'] = np.zeros(MAX_ITER)
-    h['s_norm'] = np.zeros(MAX_ITER)
-    h['eps_pri'] = np.zeros(MAX_ITER)
-    h['eps_dual'] = np.zeros(MAX_ITER)
+        return self.TOL
+    def pcp(self):
+        args = self.init_pcp()
+        for ep in range(self.n_iter):
+            args = self.episode(ep, *args)
+            if (ep == 0) or (np.mod(ep + 1,10) == 0):
+                print("%4d\t%10.4f\t%10.4f\t%10.4f\t%10.4f\t%10.2f" %(ep + 1,
+                                                                  h['err'][ep], 
+                                                                  h['eps_r'][ep], 
+                                                                  h['dz'][ep], 
+                                                                  h['eps_U'][ep], 
+                                                                  h['loss'][ep]))
+            if self.stop():
+                break
+        return h
 
-    def update_R(x, b, l):
-        return (1.0 / (1.0 + l)) * (x - b)
-    def update_S(x, b, l, g, pl):
-        return pl(x - b, l * g)
-    def update_L(x, b, l, g, pl, pm):
-        return pm(x - b, l * g, pl)
-
-    def update(func, item):
-        return map(func, [item])[0]
-
-    for k in range(MAX_ITER):
-
-        B = avg(R, S, L) - X / N + U
-
-        # Original MATLAB x-update
-        # X_1 = (1.0/(1.0+lambdap))*(X_1 - B)
-        # X_2 = prox_l1(X_2 - B, lambdap*g2)
-        # X_3 = prox_matrix(X_3 - B, lambdap*g3, prox_l1)
-
-        # Parallel x-update
-        async_R = pool.apply_async(update, (lambda x: update_R(x, B, lambdap), R))
-        async_S = pool.apply_async(update, (lambda x: update_S(x, B, lambdap, gs, prox_l1), S))
-        async_L = pool.apply_async(update, (lambda x: update_L(x, B, lambdap, gl, prox_l1, prox_matrix), L))
-
-        R = async_R.get()
-        S = async_S.get()
-        L = async_L.get()
-
-        # (for termination checks only)
-        old_z = z
-        stack = np.hstack((R, S, L))
-        diff = np.tile(X /N - avg(R, S, L), (1, N))
-        z = stack + diff
-
-        # u-update
+    def update_RSLU(self, R, S, L, U):
+        B = (R + S + L - self.X) / 3.0 + U
+        R = self.update_R(R, B)
+        S = self.update_S(S, B)
+        L = self.update_L(L, B)
         U = B
+        return R, S, L, U
 
-        # diagnostics, reporting, termination checks
-        h['loss'][k]   = loss(R, gs, S, gl, L)
-        h['r_norm'][k]   = norm(diff, 'fro')
-        h['s_norm'][k]   = norm(rho * (z - old_z), 'fro')
-        h['eps_pri'][k]  = ABSTOL + RELTOL  * np.maximum(norm(stack, 'fro'), norm(z, 'fro'))
-        h['eps_dual'][k] = ABSTOL + RELTOLN * norm(rho * U, 'fro')
+    
+    # @staticmethod
+    # def update_R(x, b, la):
+    #     return (1.0 / (1.0 + la)) * (x - b)
+    # @staticmethod
+    # def update_S(x, b, l, g, pl):
+    #     return pl(x - b, l * g)
+    # @staticmethod
+    # def update_L(x, b, l, g, pl, pm):
+    #     return pm(x - b, l * g, pl)
+    # @staticmethod
+    # def update_RSL(func, item):
+    #     return map(func, [item])[0]
 
-        if (k == 0) or (np.mod(k + 1,10) == 0):
-            print("%4d\t%10.4f\t%10.4f\t%10.4f\t%10.4f\t%10.2f" %(k + 1,
-                                                                  h['r_norm'][k], 
-                                                                  h['eps_pri'][k], 
-                                                                  h['s_norm'][k], 
-                                                                  h['eps_dual'][k], 
-                                                                  h['objval'][k]))
-        if (h['r_norm'][k] < h['eps_pri'][k]) and (h['s_norm'][k] < h['eps_dual'][k]):
-            break
+    @staticmethod
+    def prox_l1(vec, cutoff):
+        return np.maximum(0, vec - cutoff) - np.maximum(0, -vec - cutoff)
 
-    h['addm_toc'] = time.time() - start
-    h['admm_iter'] = k
-    h['R_admm'] = R
-    h['S_admm'] = S
-    h['L_admm'] = L
+    @staticmethod
+    def avg(*args):
+        return np.mean([*args], axis=0)
 
-    return h
---
+++ lv/rpca_v1.py
@@ -0,0 +1,141 @@
+import sys
+import time
+import numpy as np
+from tqdm import tqdm
+from numpy.linalg import svd, norm
+from multiprocessing.pool import ThreadPool
+
+def prox_l1(vec, cutoff):
+    return np.maximum(0, vec - cutoff) - np.maximum(0, -vec - cutoff)
+
+def prox_matrix(mat, cutoff, prox_f):
+    u, s, v = svd(mat, full_matrices=False)
+    prox_s = prox_f(s[:, None], cutoff)
+    return u.dot(np.diagflat(prox_s)).dot(v.T)
+
+def avg(*args):
+    return np.mean([*args], axis=0)
+
+def loss(R, gs, S, gl, L):
+    noise   =      norm(R, ord='fro') ** 2  # squared frobenius norm (makes X_i small)
+    sparse  = gs * norm(S.reshape(-1), ord=1)           # L1 norm (makes X_i sparse)
+    lowrank = gl * norm(L, ord="nuc")       # nuclear norm (makes X_i low rank)
+
+    return noise + sparse + lowrank
+
+
+def rpcaADMM(X, parallel=0):
+    """
+    ADMM implementation of matrix decomposition. In this case, RPCA.
+
+    Adapted from: http://web.stanford.edu/~boyd/papers/prox_algs/matrix_decomp.html
+    """
+
+
+    N = 3         # the number of matrices to split into 
+                  # (and cost function expresses how you want them)
+ 
+    # X = np.float_(data)    # A = S + L + V
+    m, n = X.shape
+    print(m, n)
+
+    gs_max = np.sum(X)      #maximum sparsity constraint
+    gl_max = norm(X, ord=2) #maximum lowrank constraint
+    gs = 0.15 * gs_max
+    gl = 0.15 * gl_max
+
+    MAX_ITER = 100
+    ABSTOL   = 1e-4 * np.sqrt(m * n * N)
+    RELTOL   = 1e-2
+    RELTOLN  = 1e-2 * np.sqrt(N)
+
+    start = time.time()
+
+    lambdap = 1.0
+    rho = 1.0 / lambdap
+
+    R = np.zeros((m, n))    #Residual
+    S = np.zeros((m, n))    #Sparse components
+    L = np.zeros((m, n))    #Low rank components
+    z = np.zeros((m, N * n))
+    U = np.zeros((m, n))
+
+    print("\n%3s\t%10s\t%10s\t%10s\t%10s\t%10s" %('iter', 'r norm', 'eps pri', 
+                                                  's norm', 'eps dual', 'loss'))
+
+    # Saving state
+    h = {}
+    h['loss'] = np.zeros(MAX_ITER)
+    h['r_norm'] = np.zeros(MAX_ITER)
+    h['s_norm'] = np.zeros(MAX_ITER)
+    h['eps_pri'] = np.zeros(MAX_ITER)
+    h['eps_dual'] = np.zeros(MAX_ITER)
+
+    def update_R(x, b, l):
+        return (1.0 / (1.0 + l)) * (x - b)
+    def update_S(x, b, l, g, pl):
+        return pl(x - b, l * g)
+    def update_L(x, b, l, g, pl, pm):
+        return pm(x - b, l * g, pl)
+
+    def update(func, item):
+        return map(func, [item])[0]
+
+    for k in range(MAX_ITER):
+
+        B = avg(R, S, L) - X / N + U
+
+        # Original MATLAB x-update
+        # X_1 = (1.0/(1.0+lambdap))*(X_1 - B)
+        # X_2 = prox_l1(X_2 - B, lambdap*g2)
+        # X_3 = prox_matrix(X_3 - B, lambdap*g3, prox_l1)
+
+        # Parallel x-update
+        if parallel:
+            pool = ThreadPool(processes=3) # Create thread pool for asynchronous processing
+
+            async_R = pool.apply_async(update, (lambda x: update_R(x, B, lambdap), R))
+            async_S = pool.apply_async(update, (lambda x: update_S(x, B, lambdap, gs, prox_l1), S))
+            async_L = pool.apply_async(update, (lambda x: update_L(x, B, lambdap, gl, prox_l1, prox_matrix), L))
+
+            R = async_R.get()
+            S = async_S.get()
+            L = async_L.get()
+        else:
+            R = update_R(R, B, lambdap)
+            S = update_S(S, B, lambdap, gs, prox_l1)
+            L = update_L(L, B, lambdap, gl, prox_l1, prox_matrix)
+
+        # (for termination checks only)
+        old_z = z
+        stack = np.hstack((R, S, L))
+        diff = np.tile(X /N - avg(R, S, L), (1, N))
+        z = stack + diff
+
+        # u-update
+        U = B
+
+        # diagnostics, reporting, termination checks
+        h['loss'][k]   = loss(R, gs, S, gl, L)
+        h['r_norm'][k]   = norm(diff, 'fro')
+        h['s_norm'][k]   = norm(rho * (z - old_z), 'fro')
+        h['eps_pri'][k]  = ABSTOL + RELTOL  * np.maximum(norm(stack, 'fro'), norm(z, 'fro'))
+        h['eps_dual'][k] = ABSTOL + RELTOLN * norm(rho * U, 'fro')
+
+        if (k == 0) or (np.mod(k + 1,10) == 0):
+            print("%4d\t%10.4f\t%10.4f\t%10.4f\t%10.4f\t%10.2f" %(k + 1,
+                                                                  h['r_norm'][k], 
+                                                                  h['eps_pri'][k], 
+                                                                  h['s_norm'][k], 
+                                                                  h['eps_dual'][k], 
+                                                                  h['loss'][k]))
+        if (h['r_norm'][k] < h['eps_pri'][k]) and (h['s_norm'][k] < h['eps_dual'][k]):
+            break
+
+    h['addm_toc'] = time.time() - start
+    h['admm_iter'] = k
+    h['R_admm'] = R
+    h['S_admm'] = S
+    h['L_admm'] = L
+
+    return h

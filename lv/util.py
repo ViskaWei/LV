281469050c27ae9de5e1842e362d3aeadd864a4c
ViskaWei <swei20@jhu.edu>
2021-11-22 10:54:44 -0500

    updates

--- lv/util.py
+++ lv/util.py
@@ -177,7 +177,6 @@ class Util():
         handles, labels = ax.get_legend_handles_labels()
         by_label = dict(zip(labels, handles))
         ax.legend(by_label.values(), by_label.keys())
-     
 
 
 
